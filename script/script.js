1e8b195a2d3d20cc749f66c14d1143a0ad9a90ab
ADAIKKAPPAN2310 <adaikkappan838@gmail.com>
2025-03-12 21:50:59 +0530

    script: logic updated

--- script/script.js
+++ script/script.js
@@ -1,52 +1,65 @@
 'use strict';
 
-let score = 5;
+let score = 7;
+let highScore = 0;
+let isPlaying = true;
 document.querySelector(".score").textContent = score;
 
-let random = Math.floor(Math.random() * 10);
+let random = Math.floor(Math.random() * 20) + 1;
 
 document.querySelector(".check").addEventListener("click", function () {
-    let value = Number(document.querySelector(".guess").value);
+    if (isPlaying) {
+        let value = Number(document.querySelector(".guess").value);
 
-    if (!value) {
-        document.querySelector(".message").textContent = "No value";
-    }
-    else {
-        if (value === random) {
-            document.querySelector(".message").textContent = "Correct number!";
-            document.querySelector("body").style.backgroundColor = "green";
+        if (!value) {
+            document.querySelector(".message").textContent = "No value";
         }
-        else if (value < random) {
-            if (score > 1) {
-                document.querySelector(".message").textContent = "Too Low";
-                score--;
-                document.querySelector(".score").textContent = score;
+        else {
+            if (value === random) {
+                document.querySelector(".message").textContent = "Correct number!";
+                document.querySelector("body").style.backgroundColor = "green";
+                document.querySelector(".number").textContent = random;
+                if (highScore < score) {
+                    highScore = score;
+                    document.querySelector(".highscore").textContent = highScore;
+                }
+                isPlaying = false;
             }
-            else {
-                document.querySelector(".score").textContent = 0;
-                document.querySelector(".message").textContent = "Gameover";
+            else if (value < random) {
+                if (score > 1) {
+                    document.querySelector(".message").textContent = "Too Low";
+                    score--;
+                    document.querySelector(".score").textContent = score;
+                }
+                else {
+                    document.querySelector(".score").textContent = 0;
+                    document.querySelector(".message").textContent = "Gameover";
+                }
             }
-        }
-        else if (value > random) {
-            if (score > 1) {
-                document.querySelector(".message").textContent = "Too High";
-                score--;
-                document.querySelector(".score").textContent = score;
-            }
-            else {
-                document.querySelector(".score").textContent = 0;
-                document.querySelector(".message").textContent = "Gameover";
-            }
+            else if (value > random) {
+                if (score > 1) {
+                    document.querySelector(".message").textContent = "Too High";
+                    score--;
+                    document.querySelector(".score").textContent = score;
+                }
+                else {
+                    document.querySelector(".score").textContent = 0;
+                    document.querySelector(".message").textContent = "Gameover";
+                }
 
+            }
         }
     }
 });
 
 //Again button
-document.querySelector(".again").addEventListener("click",function(){
-    score=5;
+document.querySelector(".again").addEventListener("click", function () {
+    score = 7;
+    isPlaying = true;
     document.querySelector(".score").textContent = score;
     document.querySelector(".message").textContent = "Start guessing...";
     document.querySelector(".guess").value = "";
-    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").textContent = "?";
+    random = Math.floor(Math.random() * 20) + 1;
 });
