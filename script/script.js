8baea076dcd87cbe5401f805822fb4311f11e557
ADAIKKAPPAN2310 <adaikkappan838@gmail.com>
2025-03-13 21:38:34 +0530

    script: varibles updated

--- script/script.js
+++ script/script.js
@@ -1,50 +1,60 @@
 'use strict';
 
+const answerBox = document.querySelector(".number");
+const message = document.querySelector(".message");
+const currentScore = document.querySelector(".score");
+const topScore = document.querySelector(".highscore");
+const inputBox = document.querySelector(".guess");
+const body = document.querySelector("body");
+const btnCheck = document.querySelector(".check");
+const btnAgain = document.querySelector(".again");
+
 let score = 7;
 let highScore = 0;
 let isPlaying = true;
-document.querySelector(".score").textContent = score;
+currentScore.textContent = score;
 
 let random = Math.floor(Math.random() * 20) + 1;
 
-document.querySelector(".check").addEventListener("click", function () {
+btnCheck.addEventListener("click", function () {
     if (isPlaying) {
-        let value = Number(document.querySelector(".guess").value);
+        let value = Number(inputBox.value);
 
         if (!value) {
-            document.querySelector(".message").textContent = "No value";
+            message.textContent = "No value";
         }
         else {
             if (value === random) {
-                document.querySelector(".message").textContent = "Correct number!";
-                document.querySelector("body").style.backgroundColor = "green";
-                document.querySelector(".number").textContent = random;
+                message.textContent = "Correct number!";
+                body.style.backgroundColor = "green";
+                answerBox.textContent = random;
+                answerBox.style.color = "green";
                 if (highScore < score) {
                     highScore = score;
-                    document.querySelector(".highscore").textContent = highScore;
+                    topScore.textContent = highScore;
                 }
                 isPlaying = false;
             }
             else if (value < random) {
                 if (score > 1) {
-                    document.querySelector(".message").textContent = "Too Low";
+                    message.textContent = "Too Low";
                     score--;
-                    document.querySelector(".score").textContent = score;
+                    currentScore.textContent = score;
                 }
                 else {
-                    document.querySelector(".score").textContent = 0;
-                    document.querySelector(".message").textContent = "Gameover";
+                    currentScore.textContent = 0;
+                    message.textContent = "Gameover";
                 }
             }
             else if (value > random) {
                 if (score > 1) {
-                    document.querySelector(".message").textContent = "Too High";
+                    message.textContent = "Too High";
                     score--;
-                    document.querySelector(".score").textContent = score;
+                    currentScore.textContent = score;
                 }
                 else {
-                    document.querySelector(".score").textContent = 0;
-                    document.querySelector(".message").textContent = "Gameover";
+                    currentScore.textContent = 0;
+                    message.textContent = "Gameover";
                 }
 
             }
@@ -53,13 +63,14 @@ document.querySelector(".check").addEventListener("click", function () {
 });
 
 //Again button
-document.querySelector(".again").addEventListener("click", function () {
+btnAgain.addEventListener("click", function () {
     score = 7;
     isPlaying = true;
-    document.querySelector(".score").textContent = score;
-    document.querySelector(".message").textContent = "Start guessing...";
-    document.querySelector(".guess").value = "";
-    document.querySelector("body").style.backgroundColor = "#222";
-    document.querySelector(".number").textContent = "?";
+    currentScore.textContent = score;
+    message.textContent = "Start guessing...";
+    inputBox.value = "";
+    body.style.backgroundColor = "#222";
+    answerBox.textContent = "?";
     random = Math.floor(Math.random() * 20) + 1;
+    answerBox.style.color = "";
 });
